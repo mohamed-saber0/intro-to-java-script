5e293d14807d9a38e0502948b488a41fcd0a7bf6
mohamed-saber0 <52870575+mohamed-saber0@users.noreply.github.com>
2021-07-29 15:03:15 +0200

    Add files via upload

---
+++ Java script/main.js
@@ -0,0 +1,30 @@
+
+function display() {
+	document.getElementById('e5tbar2').style.display = "none" ;
+	document.getElementById('e5tbar4').style.display = "none" ;
+	document.getElementById('e5tbar3').style.display = "none" ;
+	document.getElementById('e5tbar1').style.display = "block" ;
+}
+
+function display1() {
+	document.getElementById('e5tbar3').style.display = "none" ;
+	document.getElementById('e5tbar4').style.display = "none" ;
+	document.getElementById('e5tbar2').style.display = "block" ;
+	document.getElementById('e5tbar1').style.display = "none" ;
+}
+
+
+
+function display2() {
+	document.getElementById('e5tbar4').style.display = "none" ;
+	document.getElementById('e5tbar3').style.display = "block" ;
+	document.getElementById('e5tbar2').style.display = "none" ;
+	document.getElementById('e5tbar1').style.display = "none" ;
+}
+
+function display3() {
+	document.getElementById('e5tbar4').style.display = "block" ;
+	document.getElementById('e5tbar3').style.display = "none" ;
+	document.getElementById('e5tbar1').style.display = "none" ;
+	document.getElementById('e5tbar2').style.display = "none" ;
+}
